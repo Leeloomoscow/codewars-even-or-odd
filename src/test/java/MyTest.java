ecbe2b0d8515b561875a885305c46d96e58996be
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-11-01 19:32:00 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testEvenOrOdd() {
+        EvenOrOdd eoo = new EvenOrOdd();
+        assertEquals("Even", eoo.even_or_odd(6));
+        assertEquals("Odd", eoo.even_or_odd(7));
+    }
 
+    private class EvenOrOdd {
+        public String even_or_odd(int number) {
+            return (((number & 1) == 0) ? "Even" : "Odd");
+
+        }
     }
 }
